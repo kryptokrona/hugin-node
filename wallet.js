e1349ed5277d0e86cf00ce2a53b2cc354dbc6ab0
Nils Jr <nilsjr@protonmail.com>
2025-04-07 00:10:39 +0200

    Set standard pass for view wallet

--- wallet.js
+++ wallet.js
@@ -27,7 +27,7 @@ async import(password) {
   
   }
   this.wallet = wallet
-  save(password, 'nodewallet', wallet)
+  save('password', 'nodewallet', wallet)
   this.start()
   return true
 }
@@ -51,7 +51,7 @@ async import(password) {
     this.wallet.enableAutoOptimization(false);
     this.wallet.scanPoolTransactions(false);
     await this.wallet.start()
-    setInterval( async () => save(password, 'nodewallet', this.wallet), 350000);
+    setInterval( async () => save('password', 'nodewallet', this.wallet), 350000);
     this.wallet.on('incomingtx', async (transaction) => {
       console.log('Incoming tx!', transaction);
       this.txs.push(transaction)
